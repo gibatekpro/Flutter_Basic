83419929e31aa882f36cddb3678b716167387aca
Gibah Anthony <gibatekpro@gmail.com>
2023-01-30 16:47:34 +0100

    Final Commit

--- dice/lib/main.dart
+++ dice/lib/main.dart
@@ -1,25 +1,79 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(
-      MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.red,
-          appBar: AppBar(
-            backgroundColor: Colors.red,
-            title: const Text('Dice Toss'),
-          ),
-          body: const DicePage(),
-        ),
-      )
-  );
+  return runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text('Dice Toss'),
+      ),
+      body: const DicePage(),
+    ),
+  ));
 }
 
-class DicePage extends StatelessWidget {
-  const DicePage({Key? key}) : super(key: key);
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber = 1;
+  var rightDiceNumber = 6;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      tossDice();
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(0.0),
+                    child: Image.asset('images/dice$leftDiceNumber.png'),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      tossDice();
+                    });
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(0.0),
+                    child: Image.asset('images/dice$rightDiceNumber.png'),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void tossDice() {
+    leftDiceNumber = Random().nextInt(5)+1;
+    rightDiceNumber = Random().nextInt(5)+1;
   }
 }
